1a9b3bf52778af18b536093b501356d90d63e006
laiscavalcanti <cavalcantislais@gmail.com>
2020-07-16 01:36:20 -0300

    template literals

---
+++ VisaoGeralSobreECMAETS/templateLiterals.js
@@ -0,0 +1,7 @@
+//with literal templetes we can create strings without concatenating. to use we need the crase
+
+const music = {
+    name: 'start me up'
+}
+
+console.log(`This song ${music.name} is from rolling stones `)
