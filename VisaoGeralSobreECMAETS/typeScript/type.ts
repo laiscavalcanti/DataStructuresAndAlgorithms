4ff6b8620c9a12434a61fac6c4c4d956cad46587
Lais Cavalcanti <cavalcantislais@gamil.com>
2020-07-23 18:18:04 -0300

    ts

---
+++ VisaoGeralSobreECMAETS/typeScript/type.ts
@@ -0,0 +1,10 @@
+let age: number = 20;
+let existsFlag: boolean = true;
+let lenguage: string = 'Javascript';
+/*In this code assign the types variables */
+
+let age = 20; //number
+let existsFlag = true; //boolean
+let lenguage = 'Javascript'; //string
+
+/* the variables declared with value, the ts will infer what is their type */
