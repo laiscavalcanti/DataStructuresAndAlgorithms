9b61f3d1bd40b6e49fb21176f678863127bf5d50
Lais Cavalcanti <cavalcantislais@gamil.com>
2020-07-23 18:08:43 -0300

    ts

---
+++ VisaoGeralSobreECMAETS/typeScript/helloWordTs.ts
@@ -0,0 +1,6 @@
+/*Typescript is a superset of Javascript created by Microsoft.
+One of its resources is to be able to tape the variables*/
+
+
+const message: string = 'hello world'; // ts accepts to assign variable type
+console.log(message);
