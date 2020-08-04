6c28382d52bb20dbe2a37be57012db1aea764363
Lais Cavalcanti <cavalcantislais@gamil.com>
2020-08-03 21:49:50 -0300

    filter

---
+++ Arrays/arrayFilter.js
@@ -0,0 +1,7 @@
+import isEven from './arrayIteracao.js';
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const myFilter = numbers.filter(isEven);
+//the filter method returns an array of elements with which the function received true
+
