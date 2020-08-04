6e3333f1942905a0148cdc978744b990ba617425
Lais Cavalcanti <cavalcantislais@gamil.com>
2020-08-03 21:21:27 -0300

    foreach

---
+++ Arrays/arrayForEach.js
@@ -0,0 +1,7 @@
+import isEven from './arrayIteracao.js';
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+numbers.forEach(x => console.log(x % 2 === 0));
+
+//the function forEach iterates the whole array just like the loop for
