c18b5566b53136099f36efdbb730c289b7f513d4
Lais Cavalcanti <cavalcantislais@gmail.com>
2020-11-24 00:58:39 -0300

    hot potato

--- FilasEDeques/hotPotato.js
+++ FilasEDeques/hotPotato.js
@@ -1,7 +1,17 @@
 import Queue from './queue.js';
 
-function hotPotato(){
+function hotPotato(elementList, num){
     const queue = new Queue();
+    const elementList = []
 
+    for (let i = 0; i < elementList.length; i++){
+        queue.enqueue(elementList[i])
+    }
+
+    while(queue.size() > 1){
+        for(let i = 0; i < num; i++){
+            
+        }
+    }
     
 }
